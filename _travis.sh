4ba6043c3f86e7cb056dd10218749b8a4fda1493
esvwwxn <jing.e.zhang@ericsson.com>
2017-11-23 15:38:46 +0800

    update travis.sh

--- _travis.sh
+++ _travis.sh
@@ -45,8 +45,8 @@ Job NUM  | $TRAVIS_JOB_NUMBER |
 EOF
 
   git init
-  git config user.name "shenliyang"
-  git config user.email ""
+  git config user.name "zhangjingl02"
+  git config user.email "zhangjingl02@126.com"
   git add .
   git commit -m "Build by Travis CI"
   git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:master
